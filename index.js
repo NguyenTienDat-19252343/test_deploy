780d6116f732b9ddb62493e8d51effc91f862013
NguyenTienDat-19252343 <19521343@gm.uit.edu.vn>
2022-10-18 22:25:55 +0700

    update

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const morgan = require("morgan");
 const dotenv = require("dotenv");
 var bodyParser = require("body-parser");
 
+
+dotenv.config();
+
+
+mongoose.connect((process.env.mongodb_url), () =>{
+    console.log("Connected to DB");
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("common"));
+
+
+
+
+
 app.listen( 1000, () =>{
     console.log("Server is running at port 1000.");
 });
