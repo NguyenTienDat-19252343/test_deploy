8e2b55efade2f8e6a22ebf2acbb53b81be08a85a
NguyenTienDat-19252343 <19521343@gm.uit.edu.vn>
2022-10-22 16:23:35 +0700

    Update model.js

--- model/model.js
+++ model/model.js
@@ -1,2 +1,16 @@
 const mongoose = require("mongoose");
 
+const student = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  StudentID: {
+    type: Int16Array,
+    require: true,
+  },
+  birthday: {
+    type: Date,
+    reuire: true,
+  },
+});
