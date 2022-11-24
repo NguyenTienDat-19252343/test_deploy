49bef93d7ca11d427fcf939d8e0e0b6ad88fd19a
NguyenTienDat-19252343 <19521343@gm.uit.edu.vn>
2022-11-24 16:53:50 +0700

    update

--- src/config/viewengine.js
+++
@@ -1,9 +0,0 @@
-import Express from "express";
-
-const configviewengine = (app) => {
-  app.use(Express.static(__dirname + "/public"));
-  app.set("view engine", "ejs");
-  app.set("views", "./src/views");
-};
-
-export default configviewengine;
